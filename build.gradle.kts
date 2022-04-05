98283f83a9fcbc319c50eb9b1e3264cc73a59ca4
Jimmy Morales <me@jimmymorales.dev>
2022-04-05 08:44:49 -0600

    chore: Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.gradle.app) apply false
     alias(libs.plugins.android.gradle.lib) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ben.manes.versions)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "WordleX"
-include ':app'
+include(":app")
