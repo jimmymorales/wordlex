306ac6f8fcffc627eae11f905aa804c214489abe
Jimmy Morales <me@jimmymorales.dev>
2022-04-07 08:25:42 -0600

    feat: Add app scaffold composable

--- app/src/main/kotlin/dev/jimmymorales/wordlex/MainActivity.kt
+++ app/src/main/kotlin/dev/jimmymorales/wordlex/MainActivity.kt
@@ -3,12 +3,12 @@ package dev.jimmymorales.wordlex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import dev.jimmymorales.wordlex.ui.theme.WordleXTheme
 
@@ -17,27 +17,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WordleXTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                AppScaffold()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AppScaffold(
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(stringResource(id = R.string.app_name)) },
+            )
+        },
+    ) {
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     WordleXTheme {
-        Greeting("Android")
+        AppScaffold()
     }
 }
